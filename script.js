1947d6c6e53ba45146a0c7c2dbd1d370bf454741
arkadeep16 <75775078+arkadeep16@users.noreply.github.com>
2024-02-26 02:40:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-const form = document.getElementById("form1");
 function getFormvalue() {
-    Alert("form.fname.value");
+    // Get the form element
+    const form = document.getElementById("form1");
 
+    // // Get the values of the first and last name from the form
+    var fName = form.fname.value;
+    var lName = form.lname.value;
+
+    // // Concatenate the first and last name
+    var fullName = fName + " " + lName;
+
+    // Display the result using an alert
+    alert(fullName);
 }
